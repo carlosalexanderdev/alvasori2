0664ce8e80351edf54fc31a3109a42c862b0f21f
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2022-06-09 07:32:12 +0000

    Github pages test 2

---
+++ docs/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,40 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-d377055c.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "dynamicImports": [
+      "pages/index.vue",
+      "layouts/default.vue"
+    ],
+    "css": [
+      "entry.f0ddd248.css"
+    ],
+    "assets": [
+      "materialdesignicons-webfont.887a46a0.eot",
+      "materialdesignicons-webfont.e9522727.woff2",
+      "materialdesignicons-webfont.acbe194b.woff",
+      "materialdesignicons-webfont.86fcc8e4.ttf",
+      "materialdesignicons-webfont.87f88639.svg"
+    ]
+  },
+  "pages/index.vue": {
+    "file": "index-f1f84f6f.mjs",
+    "src": "pages/index.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  },
+  "layouts/default.vue": {
+    "file": "default-c53c06e8.mjs",
+    "src": "layouts/default.vue",
+    "isDynamicEntry": true,
+    "imports": [
+      "node_modules/nuxt/dist/app/entry.mjs"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
