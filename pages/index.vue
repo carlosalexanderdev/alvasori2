219cb7f15c1566b5f38f32024097bb3323d5c5cd
Carlos Alvarez <carlos.alexander.dev@gmail.com>
2022-06-14 03:43:33 +0000

    Improved theme

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-container class="d-flex justify-center mb-5">
-      <div class="text-h2">Coming soon...</div>
+      <div class="text-h2 text-primary">Coming soon...</div>
     </v-container>
     <v-img
       src="/logo/logo-no-background.png"
